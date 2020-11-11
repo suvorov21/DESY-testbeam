283f9fb96a0a7c5ba5163cd8e3e1e71e84cb688e
Sergey Suvorov <suvorov@inr.ru>
2020-11-11 17:51:49 +0100

    implement parallel processing. Not working always fine though

--- script/RunIter.py
+++ script/RunIter.py
@@ -1,50 +1,111 @@
 #!/usr/bin/env python3
 
 import os
-import random
-from sys import exit
-import subprocess
+from subprocess import run, Popen, PIPE
 
 def main():
     # input definition
-    command = 'SpatialResol.exe.test'
+    com_path = 'build'
+    command = 'SpatialResol.exe'
     flags = ['-b', '-r']
 
-    TEvent = True
-    Niter = 20
+    multithread = False
+
+    TEvent = False
+    Niter = 10
+
+    split = 10
 
     input_path  = "/eos/experiment/neutplatform/t2knd280/DESY_TPC/ROOT/v1/2019_06_14/"
-    input_name  = "R2019_06_14-17_34_58-000.root"
+    # input_path  = "/eos/user/s/ssuvorov/DESY_testbeam/tree/"
+    # input_path  = '~/DATA/'
+    input_name  = "/R2019_06_14-16_42_26-000.root"
 
-    out_path    = "/eos/user/s/ssuvorov/DESY_testbeam/"
-    out_name    = "low_gain"
-    # end of input definition
+    out_path    = "/eos/user/s/ssuvorov/DESY_testbeam/tree/"
+    # out_path    = '~/DATA/'
+    out_name    = "g_360_200_95p_nbr"
 
-    project_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + '/../bin/')
-    #command = 'bin/'+command
+    ####################################################################
+    ########### end of input definition ################################
+    ####################################################################
+
+    project_path = os.path.abspath(os.path.dirname(
+                                                   os.path.abspath(__file__)
+                                                   ) + '/../' + com_path)
     command = project_path + '/' + command
     comm = [command]
     for flag in flags:
         comm.append(flag)
 
-    for i in range (0, Niter):
+    if multithread:
+        import ROOT
+        f = ROOT.TFile(input_file, 'READ')
+        try:
+            Nentries = f.tree.GetEntries()
+        except AttributeError:
+            try:
+                Nentries = f.event_tree.GetEntries()
+            except:
+                print('Unknown tree')
+                return
+
+    for iterId in range (0, Niter):
         exe = comm.copy()
-        exe.append('-t'+str(i))
+        exe.append('-t'+str(iterId))
         # generate TEvent for the first time
+        input_file = input_path+input_name
         if TEvent:
-            if i == 0:
+            if iterId == 0:
                 exe.append('-s')
-                exe.append('-i '+input_path+input_name)
             else:
-                exe.append('-i '+out_path+input_name)
+                input_file = out_path+input_name
+
+        exe.append('-i ' + input_file)
+        output_base = out_path+out_name+'_iter'+str(iterId)
+
+        if not multithread:
+            ####### single thread run ######################################
+            exe.append('-o '+ output_base +'.root')
+            print(' '.join(exe))
+            run(exe, check=True)
+            # ##### end of run  ############################################
         else:
-            exe.append('-i '+input_path+input_name)
+            ####### multiple thread run  ###################################
+            exe_list = []
+            step = int(Nentries // split + 1)
+            start = 0
+            print('#######################################################')
+            print('#  Multithread run may provide unexpected output. WIP #')
+            print("#              Proceed with caution!!!                #")
+            print('#######################################################')
+            for thread in range(split):
+                exe_step = exe.copy()
+                exe_step.extend(['--start', str(start),
+                                '--end', str(start+step),
+                                '-o ' + output_base + '_part' + str(thread) + '.root'
+                                ])
+                exe_list.append(exe_step)
+                start += step
 
-        # add iteration number to output file
-        exe.append('-o '+out_path+out_name+'_iter'+str(i)+'.root')
+            for exe_step in exe_list:
+                print(' '.join(exe_step))
 
-        print(exe)
-        subprocess.run(exe, check=True)
+            procs_list = [Popen(cmd,
+                                stdout=PIPE,
+                                stderr=PIPE
+                                ) for cmd in exe_list]
+            for proc_id, proc in enumerate(procs_list):
+                proc.wait()
+                print(f'Done part {proc_id}')
+            force = '-f ' if '-r' in flags else ''
+            proc = Popen('hadd ' + force + output_base +'.root ' + output_base + '_part*.root',
+                         shell=True,
+                         stdout=PIPE,
+                         stderr=PIPE
+                         )
+            proc.wait()
+            print(f'Merge done iteration {iterId}')
+            ####### end of run  ############################################
 
 
 if __name__ == "__main__":
