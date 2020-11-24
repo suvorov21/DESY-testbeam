5fc804609b0ac0bcd44fafa82b1665d9263dd00a
Sergey Suvorov <suvorov@inr.ru>
2020-11-24 11:54:43 +0100

    fix a geometry problem

--- src/utils/Geom.cxx
+++ src/utils/Geom.cxx
@@ -5,7 +5,7 @@ using namespace geom;
 float geom::GetYpos(int it_y, bool invert) {
   if ((!invert && it_y >= geom::nPady) ||
       (invert && it_y >= geom::nPadx) || it_y < 0) {
-    std::cerr << "ERROR. AnalysisBase::GetYpos(). Wrong Index " <<  it_y << "\t" << invert << std::endl;
+    std::cerr << "ERROR. geom::GetYpos(). Wrong Index " <<  it_y << "\t" << invert << std::endl;
     exit(1);
   }
   if (!invert)
@@ -15,19 +15,19 @@ float geom::GetYpos(int it_y, bool invert) {
 }
 
 Float_t geom::GetXposPad(const THit* h, bool invert, Float_t angle) {
-  return geom::GetXpos(h->GetCol(invert)) * TMath::Cos(angle) -
-         geom::GetYpos(h->GetRow(invert)) * TMath::Sin(angle);
+  return geom::GetXpos(h->GetCol(invert), invert) * TMath::Cos(angle) -
+         geom::GetYpos(h->GetRow(invert), invert) * TMath::Sin(angle);
 }
 
 Float_t geom::GetYposPad(const THit* h, bool invert, Float_t angle) {
-  return   geom::GetXpos(h->GetCol(invert)) * TMath::Sin(angle) +
-           geom::GetYpos(h->GetRow(invert)) * TMath::Cos(angle);
+  return   geom::GetXpos(h->GetCol(invert), invert) * TMath::Sin(angle) +
+           geom::GetYpos(h->GetRow(invert), invert) * TMath::Cos(angle);
 }
 
 float geom::GetXpos(int it_x, bool invert) {
   if ((!invert && it_x >= geom::nPadx) ||
       (invert && it_x >= geom::nPady) || it_x < 0) {
-    std::cerr << "ERROR. AnalysisBase::GetXpos(). Wrong Index " <<  it_x << "\t" << invert << std::endl;
+    std::cerr << "ERROR. geom::GetXpos(). Wrong Index " <<  it_x << "\t" << invert << std::endl;
     exit(1);
   }
   if (!invert)
