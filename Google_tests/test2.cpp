5dcdbba8fa50c19eced1043ba3834ff720f9fb75
Sergey Suvorov <suvorovsb@gmail.com>
2021-10-13 11:03:51 +0200

    test update

--- Google_tests/test2.cpp
+++ Google_tests/test2.cpp
@@ -2,8 +2,10 @@
 // Created by SERGEY SUVOROV on 10/10/2021.
 //
 #include "gtest/gtest.h"
-#include "SpatialResolAna.hxx"
+//#include "SpatialResolAna.hxx"
 #include "AnalysisBase.hxx"
+
+#include "TRawEvent.hxx"
 
 #include "Geom.hxx"
 
@@ -12,14 +14,28 @@ TEST(Initialisation, outputWrite) {
   EXPECT_EQ(ana->WriteOutput(), false);
 }
 
-TEST(GeomDeathTest, OferflowX) {
+TEST(GeomThrowTest, OferflowX) {
   EXPECT_THROW(geom::GetXpos(geom::GetNColumn() + 1),
               std::logic_error
                );
 }
 
-TEST(GeomDeathTest, OferflowY) {
+TEST(GeomThrowTest, OferflowY) {
   EXPECT_THROW(geom::GetYpos(geom::GetNColumn() + 1),
                std::logic_error
                );
+}
+
+TEST(CtorTests, hitCtor) {
+  auto hit = new THit();
+  hit->SetADC(1, 10);
+  hit->SetADC(1000, 10);
+
+  ASSERT_EQ(hit->GetADC(1), 10);
+  ASSERT_EQ(hit->GetADC(1000), 0);
+}
+
+TEST(CtorTests, eventCtor) {
+  auto event = new TRawEvent();
+  ASSERT_EQ(event->GetID(), 0);
 }
